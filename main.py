d4a249d99851eeb8d967d372ff2cd1e76c40271d
vighnesh-nayak-203 <vighneshnayak203@gmail.com>
2023-12-04 12:56:47 +0530

    Tester built

--- main.py
+++ main.py
@@ -1,71 +1,188 @@
 import cv2
 import numpy as np
 
+
+def ImageLoader(img):
+    """
+    Load an image from either a file path or a NumPy array.
+
+    Parameters:
+    - img: str or numpy.ndarray
+        If str, the file path of the image. If numpy.ndarray, the image itself.
+
+    Returns:
+    - Tuple[str, numpy.ndarray]
+        A tuple containing the image path (if applicable) and the loaded image as a NumPy array.
+    """
+    image = img
+    imgPath = None
+
+    if type(img) == str:
+        imgPath = img
+        image = cv2.imread(imgPath, 0)
+
+    image.dtype = np.uint8
+    return imgPath, image
+
+
 class Encoder:
-    def __init__(self, img, watermark, blockSize=2, plane=1):
-        self.plane=plane
-        self.blockSize=blockSize
+    """
+    For encoding a watermark into an image using a block-based approach.
 
-        self.img=img
-        if type(img)==str:
-            self.imgPath=img
-            self.img=cv2.imread(self.imgPath,0)
-        self.h,self.w=self.img.shape
+    Attributes:
+    - blockSize: int
+        Size of each block for encoding.
+    - plane: int
+        Bit plane used for encoding.
+    """
 
-        try:
-            self.watermark=watermark
-        except:
-            self.watermarkPath=watermark
-            self.watermark=cv2.imread(self.watermarkPath,0)
-        self.watermark=cv2.resize(self.watermark,(self.h//self.blockSize,self.w//self.blockSize))
-        _,self.watermark=cv2.threshold(self.watermark,127,255,cv2.THRESH_BINARY)
-        self.h_w,self.w_w=self.watermark.shape
+    def __init__(self, blockSize: int = 2, plane: int = 1):
+        """
+        Initialize the Encoder with the specified block size and bit plane.
 
-        self.img.dtype=np.uint8
-        self.watermark.dtype=np.uint8
+        Parameters:
+        - blockSize: int, optional
+            Size of each block for encoding. Default is 2.
+        - plane: int, optional
+            Bit plane used for encoding. Default is 1.
+        """
+        self.plane = plane-1
+        self.blockSize = blockSize
 
-        assert (self.h_w==self.h//self.blockSize and self.w_w==self.w//self.blockSize),\
-                f'Image and watermark shapes of {self.img.shape} and {self.watermark.shape} are in compatible.'
-    
-    def encodeBlockBit(self,row,col):
-        enc=self.watermark[row,col]
-        img_block=self.img[row*self.blockSize:(row+1)*self.blockSize,
-                           col*self.blockSize:(col+1)*self.blockSize]
-        enc_block=enc*np.ones_like(img_block,'uint8')
-        masked_img=img_block & (255 - 1<<self.plane)
-        masked_enc=enc_block & (1<<self.plane)
-        return masked_img+masked_enc
-    
-    def encode(self):
-        encodedImg=np.zeros_like(self.img)
+    def getImage(self, img):
+        """
+        Load the image to be encoded and extract its dimensions.
+
+        Parameters:
+        - img: str or numpy.ndarray
+            If str, the file path of the image. If numpy.ndarray, the image itself.
+        """
+        self.imagePath, self.img = ImageLoader(img)
+        self.h, self.w = self.img.shape
+
+    def getWatermark(self, watermark):
+        """
+        Load the watermark image, resize it, and convert it to binary.
+
+        Parameters:
+        - watermark: str or numpy.ndarray
+            If str, the file path of the watermark image. If numpy.ndarray, the watermark itself.
+        """
+        self.watermarkPath, self.watermark = ImageLoader(watermark)
+
+        self.watermark = cv2.resize(
+            self.watermark, (self.w//self.blockSize, self.h//self.blockSize))
+        _, self.watermark = cv2.threshold(
+            self.watermark, 127, 255, cv2.THRESH_BINARY)
+
+        self.h_w, self.w_w = self.watermark.shape
+
+    def encodeBlockBit(self, row: int, col: int):
+        """
+        Encode a single bit of the watermark into a block of the image.
+
+        Parameters:
+        - row: int
+            Row index of the block.
+        - col: int
+            Column index of the block.
+
+        Returns:
+        - numpy.ndarray
+            Encoded block of the image.
+        """
+        enc = self.watermark[row, col]
+        img_block = self.img[row*self.blockSize:(row+1)*self.blockSize,
+                             col*self.blockSize:(col+1)*self.blockSize]
+        enc_block = enc*np.ones_like(img_block, 'uint8')
+        masked_img = img_block & (255 - 1 << self.plane)
+        masked_enc = enc_block & (1 << self.plane)
+        return masked_img + masked_enc
+
+    def encode(self, img, watermark):
+        """
+        Encode the watermark into the image.
+
+        Parameters:
+        - img: str or numpy.ndarray
+            If str, the file path of the image. If numpy.ndarray, the image itself.
+        - watermark: str or numpy.ndarray
+            If str, the file path of the watermark image. If numpy.ndarray, the watermark itself.
+
+        Returns:
+        - numpy.ndarray
+            Encoded image.
+        """
+        self.getImage(img)
+        self.getWatermark(watermark)
+
+        assert (self.h_w == self.h//self.blockSize and self.w_w == self.w//self.blockSize), \
+            f'Image and watermark shapes of {self.img.shape} and {self.watermark.shape} are incompatible.'
+
+        encodedImg = np.zeros_like(self.img)
         for col in range(self.w_w):
             for row in range(self.h_w):
                 encodedImg[row*self.blockSize:(row+1)*self.blockSize,
-                           col*self.blockSize:(col+1)*self.blockSize]=self.encodeBlockBit(row,col)
+                           col*self.blockSize:(col+1)*self.blockSize] = self.encodeBlockBit(row, col)
         return encodedImg.astype('uint8')
-    
+
 
 class Decoder:
-    def __init__(self,img, blockSize=2, plane = 1):
-        self.img=img
-        self.plane=plane
-        self.blockSize=blockSize
-        if type(img)==str:
-            self.imgPath=img
-            self.img=cv2.imread(self.imgPath,0)
-        self.h,self.w=self.img.shape
+    """
+    For decoding a watermark from an encoded image using a block-based approach.
 
-    def decode(self):
-        self.h_w,self.w_w=self.h//self.blockSize,self.w//self.blockSize
-        watermark=np.zeros((self.h_w,self.w_w))
-        img_plane=self.img & (1<<self.plane)
+    Attributes:
+    - blockSize: int
+        Size of each block for decoding.
+    - plane: int
+        Bit plane used for decoding.
+    """
+
+    def __init__(self, blockSize=2, plane=1):
+        """
+        Initialize the Decoder with the specified block size and bit plane.
+
+        Parameters:
+        - blockSize: int, optional
+            Size of each block for decoding. Default is 2.
+        - plane: int, optional
+            Bit plane used for decoding. Default is 1.
+        """
+        self.plane = plane-1
+        self.blockSize = blockSize
+
+    def getImage(self, img):
+        """
+        Load the image to be decoded and extract its dimensions.
+
+        Parameters:
+        - img: str or numpy.ndarray
+            If str, the file path of the image. If numpy.ndarray, the image itself.
+        """
+        self.imagePath, self.img = ImageLoader(img)
+        self.h, self.w = self.img.shape
+        self.h_w, self.w_w = self.h//self.blockSize, self.w//self.blockSize
+
+    def decode(self, img):
+        """
+        Decode the watermark from the encoded image.
+
+        Parameters:
+        - img: str or numpy.ndarray
+            If str, the file path of the encoded image. If numpy.ndarray, the encoded image itself.
+
+        Returns:
+        - numpy.ndarray
+            Decoded watermark.
+        """
+        self.getImage(img)
+        watermark = np.zeros((self.h_w, self.w_w))
+
+        img_plane = self.img & (1 << self.plane)
         for col in range(self.w_w):
             for row in range(self.h_w):
-                    img_block=img_plane[row*self.blockSize:(row+1)*self.blockSize,
-                              col*self.blockSize:(col+1)*self.blockSize]
-                    # print(img_block)
-                    enc_byte=255*np.round(img_block.mean(),0)
-                    watermark[row,col]=enc_byte
+                img_block = img_plane[row*self.blockSize:(row+1)*self.blockSize,
+                                      col*self.blockSize:(col+1)*self.blockSize]
+                enc_byte = 255 * np.round(img_block.mean(), 0)
+                watermark[row, col] = enc_byte
         return watermark.astype('uint8')
-
-
--- tester.py
+++ tester.py
@@ -1,51 +1,194 @@
 import numpy as np
-import cv2
+import os
 import matplotlib.pyplot as plt
-from main import Encoder, Decoder
-
-def MSE(img1,img2):
-    assert img1.shape==img2.shape, "Image sizes doesn't match."
-    w,h=img1.shape
-    return ((img1-img2)**2).sum()/(w*h)
-
-def PSNR(img1,img2):
-    return 10*np.log10(255*255/MSE(img1,img2))
-
-def NCC(img1,img2):
-    assert img1.shape==img2.shape, "Image sizes doesn't match."
-    w,h=img1.shape
-    CC=((img1-img1.mean())*(img2-img2.mean())).sum()/(w*h)
-    return CC/(img1.std()*img2.std())
-
-def test(imgPath,wmPath, enc_plane=0, block_size=2):
-    img=cv2.imread(imgPath,0)
-    wm=cv2.imread(wmPath,0)
-
-    encoder=Encoder(img,wm,block_size,enc_plane) 
-    encoded_image=encoder.encode()
-    wm=encoder.watermark
-
-    noise=0.5*np.random.randn(*encoded_image.shape)
-    encoded_image_gaussian_noise=np.uint8(np.round(encoded_image+noise,0))
-
-    decoder=Decoder(encoded_image_gaussian_noise,block_size,enc_plane)
-    decoded_watermark=decoder.decode()
-
-    return PSNR(img,encoded_image),NCC(wm,decoded_watermark)
+from main import Encoder, Decoder, ImageLoader
 
 
-img='imgs/barbara256.png'
-wm1='watermarks/10.png'
-wm2='watermarks/watermark.jpg'
+class Metrics:
+    """
+    Class for computing image quality metrics.
 
-PSNRs=[]
-NCCs=[]
+    Attributes:
+    - None
+    """
 
-for i in range(1,6):
-    psnr,ncc=test(img,wm1,block_size=i)
+    def getImages(self, img1, img2):
+        """
+        Load two images and ensure their sizes match.
+
+        Parameters:
+        - img1: str or numpy.ndarray
+            If str, the file path of the first image. If numpy.ndarray, the first image itself.
+        - img2: str or numpy.ndarray
+            If str, the file path of the second image. If numpy.ndarray, the second image itself.
+
+        Returns:
+        - Tuple[numpy.ndarray, numpy.ndarray]
+            A tuple containing the loaded first and second images.
+        """
+        img1Path, img1 = ImageLoader(img1)
+        img2Path, img2 = ImageLoader(img2)
+        assert img1.shape == img2.shape, "Image sizes don't match."
+        return img1, img2
+
+    def MSE(self, img1, img2):
+        """
+        Compute the Mean Squared Error (MSE) between two images.
+
+        Parameters:
+        - img1: str or numpy.ndarray
+            If str, the file path of the first image. If numpy.ndarray, the first image itself.
+        - img2: str or numpy.ndarray
+            If str, the file path of the second image. If numpy.ndarray, the second image itself.
+
+        Returns:
+        - float
+            The computed MSE value.
+        """
+        img1, img2 = self.getImages(img1, img2)
+        h, w = img1.shape
+        return ((img1 - img2) ** 2).sum() / (w * h)
+
+    def PSNR(self, img1, img2):
+        """
+        Compute the Peak Signal-to-Noise Ratio (PSNR) between two images.
+
+        Parameters:
+        - img1: str or numpy.ndarray
+            If str, the file path of the first image. If numpy.ndarray, the first image itself.
+        - img2: str or numpy.ndarray
+            If str, the file path of the second image. If numpy.ndarray, the second image itself.
+
+        Returns:
+        - float
+            The computed PSNR value.
+        """
+        return 10 * np.log10(255 ** 2 / self.MSE(img1, img2))
+
+    def NCC(self, img1, img2):
+        """
+        Compute the Normalized Cross-Correlation (NCC) between two images.
+
+        Parameters:
+        - img1: str or numpy.ndarray
+            If str, the file path of the first image. If numpy.ndarray, the first image itself.
+        - img2: str or numpy.ndarray
+            If str, the file path of the second image. If numpy.ndarray, the second image itself.
+
+        Returns:
+        - float
+            The computed NCC value.
+        """
+        img1, img2 = self.getImages(img1, img2)
+        h, w = img1.shape
+        CC = ((img1 - img1.mean()) * (img2 - img2.mean())).sum() / (w * h)
+        return CC / (img1.std() * img2.std())
+
+
+class Tester:
+    """
+    Class for testing the encoder and decoder with various parameters.
+
+    Attributes:
+    - imageDir: str
+        Directory containing the original images.
+    - watermarkDir: str
+        Directory containing the watermarks.
+    - plane: int
+        Bit plane used for encoding/decoding.
+    - blocksize: int
+        Size of each block for encoding/decoding.
+    """
+
+    def __init__(self, imageDir: str, watermarkDir: str, plane: int = 1, blocksize: int = 2) -> None:
+        """
+        Initialize the Tester with the specified directories and parameters.
+
+        Parameters:
+        - imageDir: str
+            Directory containing the original images.
+        - watermarkDir: str
+            Directory containing the watermarks.
+        - plane: int, optional
+            Bit plane used for encoding/decoding. Default is 1.
+        - blocksize: int, optional
+            Size of each block for encoding/decoding. Default is 2.
+        """
+        self.imageDir = imageDir
+        self.watermarkDir = watermarkDir
+        self.images = os.listdir(self.imageDir)
+        self.watermarks = os.listdir(self.watermarkDir)
+        self.metrics = Metrics()
+        self.encoder = Encoder(blocksize, plane)
+        self.decoder = Decoder(blocksize, plane)
+
+    def unitTest(self, imageIdx: int, watermarkIdx: int):
+        """
+        Perform a unit test with a specific pair of images and watermarks.
+
+        Parameters:
+        - imageIdx: int
+            Index of the image in the 'images' directory.
+        - watermarkIdx: int
+            Index of the watermark in the 'watermarks' directory.
+
+        Returns:
+        - Tuple[float, float]
+            A tuple containing the computed PSNR and NCC values.
+        """
+        _, img = ImageLoader(os.path.join(
+            self.imageDir, self.images[imageIdx]))
+        _, wm = ImageLoader(os.path.join(self.watermarkDir,
+                            self.watermarks[watermarkIdx]))
+
+        encoded_image = self.encoder.encode(img, wm)
+
+        noise = 0.5 * np.random.randn(*encoded_image.shape)
+        encoded_image_gaussian_noise = np.uint8(
+            np.round(encoded_image + noise, 0))
+
+        decoded_wm = self.decoder.decode(encoded_image_gaussian_noise)
+
+        psnr = self.metrics.PSNR(img, encoded_image)
+        ncc = self.metrics.NCC(self.encoder.watermark, decoded_wm)
+
+        return psnr, ncc
+
+    def CompleteTest(self):
+        """
+        Perform a complete test for all combinations of images and watermarks.
+
+        Returns:
+        - Tuple[float, float]
+            A tuple containing the average PSNR and NCC values across all pairs.
+        """
+        PSNR_Tot = 0
+        NCC_Tot = 0
+        num_pairs = 0
+
+        for i in range(len(self.images)):
+            for j in range(len(self.watermarks)):
+                psnr, ncc = self.unitTest(i, j)
+                PSNR_Tot += psnr
+                NCC_Tot += ncc
+                num_pairs += 1
+
+        return PSNR_Tot / num_pairs, NCC_Tot / num_pairs
+
+
+# Example usage
+imgDir = 'imgs'
+wmDir = 'watermarks'
+
+PSNRs = []
+NCCs = []
+
+for i in range(1, 9):
+    tester = Tester(imgDir, wmDir, i, 4)
+    psnr, ncc = tester.CompleteTest()
     PSNRs.append(psnr)
     NCCs.append(ncc)
 
-x=np.arange(1,6)
-plt.plot(x,NCCs)
+x = np.arange(1, 9)
+plt.plot(x, PSNRs)
 plt.show()
